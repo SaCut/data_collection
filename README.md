d34bcf7ca78f7766f532af19ab40963e77ae2fc5
Saverio <saveriocutrupi@users.noreply.github.com>
2021-03-23 12:12:57 +0000

    Created repository, added README.md, .gitignore, lists&tuples.py

---
+++ README.md
@@ -0,0 +1,7 @@
+# Lists and Tuples
+
+### What is a list?
+- A list is a collection of elements
+- Lists in Python are mutable
+- The syntax `list_name = []` is used to create a list
+
---
+++ lists&tuples.py
@@ -0,0 +1,9 @@
+# creating a list
+
+shopping_list = ["bread", "chocolate", "avocados", "milk"]
+
+# print(shopping_list)
+# print(type(shopping_list))
+
+# list indexing
+print(shopping_list[0])
