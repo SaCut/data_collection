e0d09fc1b255de93bdd6a9836cec2a86fc3a8d00
Saverio <saveriocutrupi@users.noreply.github.com>
2021-03-23 12:31:38 +0000

    updated README.md, .gitignore and lists&tuples.py

--- README.md
+++ README.md
@@ -4,4 +4,18 @@
 - A list is a collection of elements
 - Lists in Python are mutable
 - The syntax `list_name = []` is used to create a list
+- In Python, lists can contain different data types
+
+### List methods
+- Indexing
+- `list_name[0]` gives back the 0th value in list
+
+- Changing an item
+- `list_name[0] = "element"` changes the 0th value to something different
+
+- Adding an item
+- `list_name.append(variable)` adds an element to the end of the list
+
+- Removing an item
+- `list_name.remove("element")` or `list_name.remove(list_name[0])` removes the specified element from the list
 
--- lists&tuples.py
+++ lists&tuples.py
@@ -1,9 +1,25 @@
 # creating a list
 
 shopping_list = ["bread", "chocolate", "avocados", "milk"]
+			  #		0			1			2		  3
 
 # print(shopping_list)
 # print(type(shopping_list))
 
 # list indexing
-print(shopping_list[0])
+# print(shopping_list[0])
+
+# change a value in the list
+shopping_list[0] = "orange"
+print(shopping_list)
+
+# add a value to a list
+shopping_list.append("ice-cream")
+print(shopping_list)
+
+# remove an item from a list
+shopping_list.remove(shopping_list[0])
+print(shopping_list)
+
+# can we mix data types in a list? yes
+open_minded_list = [1, 2, 3, "one", "two", "three"]
